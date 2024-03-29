f779d5649e3d4cf5450aa4570215dc171827fc5b
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-03-29 15:04:56 +0630

    add: setup router and components file in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Box } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" exact element={<VideoDetail />} />
+          <Route path="/channel/:id" exact element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 }
 
 export default App;
