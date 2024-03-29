785bec2327a61728c89f8505dba708ba9f0e927f
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-03-30 00:12:24 +0630

    add: style and get api request for channelCard

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { Link } from "react-router-dom";
 
-const ChannelCard = (channel) => {
-  return <div>ChannelCard</div>;
+const ChannelCard = ({ channel }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: { xs: "356px", md: "320px" },
+        height: "326px",
+        margin: "auto",
+        boxShadow: "none",
+        borderRadius: "20px",
+      }}
+    >
+      <Link to={`/channel/${channel?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            textAlign: "center",
+            flexDirection: "column",
+            color: "#fff",
+            gap: 1,
+          }}
+        >
+          <CardMedia
+            image={channel?.snippet?.thumbnails?.high?.url}
+            sx={{
+              width: "180px",
+              height: "180px",
+              borderRadius: "50%",
+              border: "1px solid #e3e3e3",
+            }}
+          />
+          <Typography variant="h6">
+            {channel?.snippet?.channelTitle}
+            <CheckCircleIcon
+              sx={{ fontSize: "15px", color: "gray", ml: "5px" }}
+            />
+          </Typography>
+          {channel?.statistics?.subscriberCount && (
+            <Typography
+              sx={{ fontSize: "15px", fontWeight: "500", color: "gray" }}
+            >
+              {parseInt(channel?.statistics?.subscriberCount).toLocaleString(
+                "en-US"
+              )}
+              Subscribers
+            </Typography>
+          )}
+        </CardContent>
+      </Link>
+    </Box>
+  );
 };
 
 export default ChannelCard;
